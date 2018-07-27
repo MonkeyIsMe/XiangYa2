203f887a17c4f37c3447fe9319f6ef7a465f6ac1
CallMeDad <CallMeDad@LAPTOP-FOL3OPE7>
2018-07-27 10:19:02 +0800

    ClientDAO测试完成

--- src/com/csu/test/TestClientInfo.java
+++ src/com/csu/test/TestClientInfo.java
@@ -1,5 +1,38 @@
 package com.csu.test;
 
-public class TestClientInfo {
+import java.util.ArrayList;
 
+import org.junit.Test;
+
+import com.csu.dao.ClientInfoDAO;
+import com.csu.entity.PatientInfo;
+
+public class TestClientInfo {
+	
+	@Test
+	public void searchCount() {
+		String pname = "·½·¼·¼";
+		ClientInfoDAO cid = new ClientInfoDAO();
+		int ans = cid.searchCount(pname);
+		System.out.println(ans);
+	}
+	
+	@Test
+	public void count() {
+		ClientInfoDAO cid = new ClientInfoDAO();
+		System.out.println(cid.count());
+	}
+	
+	@Test
+	public void SelectClientInfoByName() {
+		ClientInfoDAO cid = new ClientInfoDAO();
+		int i = 0;
+		int pageSize = 2;
+		String pname = "·½·¼·¼";
+		ArrayList<PatientInfo> pi = cid.SelectClientInfoByName(i, pageSize, pname);
+		for(PatientInfo pid :pi) {
+			System.out.println(pid.toString());
+		}
+		
+	}
 }
